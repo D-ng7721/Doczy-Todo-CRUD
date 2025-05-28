3d77a667f24a534e36eb663cc610fda8ec987089
D-ng1277 <haidangbachkhoa@gmail.com>
2025-05-29 01:16:27 +0700

    feat: todo's filter uses partial match for name and content
    The Todo's repository will now use partial match on title and content.

--- src/modules/todo/repos/todo.repository.ts
+++ src/modules/todo/repos/todo.repository.ts
@@ -4,6 +4,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { FilterQuery, Model, SortOrder } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { EntityNotFound } from 'src/common/errors/EntityNotFound.error';
+import { Status } from '../types/status.enum';
 
 @Injectable()
 export class TodoRepository implements IRepository<Todo> {
@@ -28,8 +29,29 @@ export class TodoRepository implements IRepository<Todo> {
     sortBy: string = '_id',
     sortOrder: SortOrder = 'desc',
   ): Promise<Todo[]> {
+    const filter: FilterQuery<Todo> = {};
+
+    // Include match for title
+    if (query.title) {
+      filter.title = { $regex: query.title as string, $options: 'i' }; // case-insensitive include match
+    }
+
+    // Include match for content
+    if (query.content) {
+      filter.content = { $regex: query.content as string, $options: 'i' };
+    }
+
+    // Copy over other exact-match fields
+    if (query.status) {
+      filter.status = query.status as Status;
+    }
+
+    if (query.dateTime) {
+      filter.dateTime = query.dateTime as string;
+    }
+
     const foundTodos = await this.todoModel
-      .find(query)
+      .find(filter)
       .sort({ [sortBy]: sortOrder })
       .skip(pagination.skip)
       .limit(pagination.limit)
